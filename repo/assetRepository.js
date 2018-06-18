8bf90966a1d99da28582795ce1c1b44f30573835
kirstyannepollock <kpollock@hotmail.co.uk>
2018-06-18 06:56:37 +0200

    comments

--- repo/assetRepository.js
+++ repo/assetRepository.js
@@ -31,7 +31,7 @@ class AssetRepository {
 
     create(asset) {
         //assumes the client call sends also the content. 
-        //TO DO: save as file!!!
+        //TO DO: save as file!!!Cam9ge
         var hash = hashing.getHash(
             {
                 content: asset.content,
